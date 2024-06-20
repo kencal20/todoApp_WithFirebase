8baa180d492e0b88ca954400cbd99444ada6eb11
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-20 08:53:53 +0000

    adding the  addButton  && its styles

--- src/pages/homeScreen.jsx
+++ src/pages/homeScreen.jsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={styles.addItemButtonContainer}>
+                <TouchableOpacity style={styles.addItemButton}>
+                    <Text style={styles.addItemButtonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#F5F5F5',
+        flex: 1
+    },
+    addItemButtonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+    },
+    addItemButton: {
+        backgroundColor: 'dodgerblue',
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50,
+    },
+    addItemButtonText: {
+        fontSize: 20,
+    }
+})
