348825d6805d6ac6f8d83f60e9d76dfa330a749e
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-20 08:29:40 +0000

    AddTask Screen init

---
+++ src/pages/addTaskScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddTaskScreen() {
+  return (
+    <View>
+      <Text>addTaskScreen</Text>
+    </View>
+  )
+}
