383a6121de2815fdb4e18d5e51be5a2da55d42dc
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-20 08:33:30 +0000

    stackNavigator update

--- src/routes/stackNavigator.js
+++ src/routes/stackNavigator.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeScreen from '../pages/homeScreen'
+import DetailsScreen from '../pages/detailsScreen'
+import AddTaskScreen from '../pages/addTaskScreen'
 
 export default function StackNavigator() {
+    const Stack = createStackNavigator()
   return (
-    <View>
-      <Text>stackNavigator</Text>
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen name='home' component={HomeScreen}/>
+        <Stack.Screen name='detailsScreen' component={DetailsScreen}/>
+        <Stack.Screen name='addTask' component={AddTaskScreen}/>
+    </Stack.Navigator>
+  </NavigationContainer>
   )
 }
